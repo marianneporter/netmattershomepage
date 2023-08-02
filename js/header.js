bd9deaf8ab29b668710f3cc64b7ccc9ae221634d
marianneporter <mariannneporter17@gmail.com>
2023-08-03 00:03:05 +0100

    completed sticky menu

--- js/header.js
+++ js/header.js
@@ -1,41 +1,25 @@
 const headerGroup = document.querySelector('.header-group');
 const header      = document.querySelector('.header');
-//const nav         = document.querySelector('nav');
+const overlay     = document.querySelector('.overlay');
+const body        = document.querySelector('body');
 
-
-// let navRect       = header.getBoundingClientRect();
-// console.log(headerRect);
-// console.log(navRect);
-
-
+// store currebt scroll position as as last scroll position so you can
+// keep track of whether user is scrolling up or down
 let lastScrollY = window.scrollY;
 
 window.addEventListener("scroll", () => {
-   
-
-    if (lastScrollY > window.scrollY) {
-        console.log('going up!');
-        let headerRect = header.getBoundingClientRect();
-
-        console.log(headerRect);
-        let leftPos = (window.innerWidth - headerRect.width) / 2;
-        console.log(leftPos);
-      
-        console.log(leftPos);
+    
+    /*****************************************************************/
+    /* test scroll direction and initiate sticky header if this is   */
+    /* down                                                          */
+    /*****************************************************************/
+    if (lastScrollY > window.scrollY) { 
+        
         headerGroup.style.position = "fixed";
         headerGroup.style.left=0;
         headerGroup.style.right=0;
-        headerGroup.style.top=0;
-        headerGroup.style.maxHeight = "13.1rem"
-        headerGroup.style.overflowY = "hidden";
-       
-     
-
-        // headerGroup.style.top = 0;
-        // headerGroup.style.left = `${leftPos}px`;
-        // headerGroup.style.backgroundColor = "white";
-
-        headerGroup.style.zIndex = 999;
+        headerGroup.style.top=window.screenTop; 
+        headerGroup.style.zIndex = 999;     
     }
 
     if (window.scrollY === 0) {
@@ -47,3 +31,63 @@ window.addEventListener("scroll", () => {
     lastScrollY = window.scrollY;
 })
 
+
+/*****************************************************************/
+/*   add event listeners to items of main nav to enable display  */
+/*   of sticky menu and to control the dropdown menu             */
+/*************************************************************** */
+
+const navItems = document.querySelectorAll('.main-nav-item');
+
+
+navItems.forEach(el => {
+
+    el.addEventListener("mouseenter", (e) => {   
+        // display dropdown nav and prevent scrolling while dropdown nav is displayed
+        disableScroll();
+       
+        let currentHeaderDropdown = 
+                        e.target.querySelector('.main-nav-lg-dropdown');
+
+                
+        let currArrow = e.target.querySelector('.arrow');
+       
+        currentHeaderDropdown.style.top = "13.1rem";
+        currentHeaderDropdown.style.zIndex = "1";    
+        
+        let currentRect = e.target.getBoundingClientRect();
+    
+        currArrow.style.top = "0";
+        currArrow.style.left = `${currentRect.x}px`;   
+        currArrow.style.zIndex = "2";      
+    });
+
+    el.addEventListener("mouseleave", (e) => {     
+       // hide dropdown nav and re-enable scrolling
+        enableScroll();    
+        let currentHeaderDropdown 
+                   = e.target.querySelector('.main-nav-lg-dropdown');
+        currentHeaderDropdown.style.top = "-40rem";
+        currentHeaderDropdown.style.zIndex = "-1";  
+        body.style.position = "static";  
+
+    });
+})
+
+function disableScroll() {
+    // Get the current page scroll position
+    scrollTop = document.documentElement.scrollTop;
+ 
+    // if any scroll is attempted,
+    // set this to the previous value
+    window.onscroll = function() {
+        window.scrollTo(0, scrollTop);
+    };
+}
+  
+function enableScroll() {
+    window.onscroll = function() {};
+}
+
+
+  
