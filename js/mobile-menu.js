b83054f5e25b90396d15f00b63532caa66b029af
marianneporter <mariannneporter17@gmail.com>
2023-08-03 22:56:23 +0100

    wip mobile nav, added overlay with click event to close nav

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -4,16 +4,18 @@ const menuButton     = document.querySelector('.menu-icon');
 const contentOverlay = document.querySelector('.content-overlay');
 
 
+// menu button has been clicked so main content slides over to reveal mobile menu
 menuButton.addEventListener('click', () => {
-    console.log('menu button clicked');
-    if (mainContent.classList.contains('mobile-nav-active')) {
-        mainContent.classList.remove('mobile-nav-active');
-        contentOverlay.classList.add('content-overlay-active');
-    } else {
         mainContent.classList.add('mobile-nav-active');
-        contentOverlay.classList.add('content-overlay-active');
-    }
+        mainContent.style.right = "21rem";
+        contentOverlay.style.opacity ="0.5";
+        contentOverlay.style.zIndex = "10";
+ 
 })
 
-
-
+//overlay has been clicked so page slides back over the mobile menu
+contentOverlay.addEventListener('click', (e) => {  
+    mainContent.style.right="0";
+    contentOverlay.style.opacity = "0";
+    setTimeout(() => contentOverlay.style.zIndex = "-1", 2000);
+})
