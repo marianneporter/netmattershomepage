0f247a223f008ae869f614693bf9954c516af44e
marianneporter <mariannneporter17@gmail.com>
2023-08-06 00:44:15 +0100

    hamburger icon animation completed

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,31 +1,25 @@
 const mainContent    = document.querySelector('.all-content');
 const mobileMenu     = document.querySelector('.mobile-menu');
 
-const menuButton     = document.querySelector('.menu-icon');
-const menuButtonCheckbox = document.querySelector('.menu-icon input');
-const menuCheckbox = document.getElementById('#menu-checkbox');
+const hamburgerMenuButton = document.querySelector('.hamburger-menu-button');
 
 const contentOverlay = document.querySelector('.content-overlay');
 
-
-
-
-
 // menu button has been clicked so main content slides over to reveal mobile menu
-menuButton.addEventListener('click', () => {
-        console.log('menu button clicked');
-        menuButtonCheckbox.checked = "checked";
-        console.log('just set checkbox to true');
+hamburgerMenuButton.addEventListener('click', () => {       
         mainContent.style.right = "21rem";
         mainContent.style.position = "fixed";
         contentOverlay.style.opacity ="0.5";
-        contentOverlay.style.zIndex = "10"; 
+        contentOverlay.style.zIndex = "10";  
+        
+        hamburgerMenuButton.classList.add("hamburger-menu-expanded");
 })
 
 //overlay has been clicked so page slides back over the mobile menu
 contentOverlay.addEventListener('click', (e) => {  
     mainContent.style.right = "1rem";
 
+    hamburgerMenuButton.classList.remove("hamburger-menu-expanded");
     setTimeout(() => {    
 
         mainContent.style.right = "0";
@@ -36,14 +30,5 @@ contentOverlay.addEventListener('click', (e) => {
     contentOverlay.style.opacity = "0";
     setTimeout(() => contentOverlay.style.zIndex = "-1", 2000);
 
-    console.log('setting menu button icon checked to false');
-  //  console.log(menuCheckbox);
-    console.log(menuButtonCheckbox);
-    menuButtonCheckbox.checked = "false";
-    console.log(menuButtonCheckbox);
-})
-
-//********************************************************* */
-// hamburger menu functionality                             */
-//********************************************************* */
+});
 
