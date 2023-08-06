b2f6890d6d8606ddc363c9783750b283c315628d
marianneporter <mariannneporter17@gmail.com>
2023-08-06 23:19:13 +0100

    wip slick slider added photos

--- js/carousel.js
+++ js/carousel.js
@@ -3,7 +3,12 @@ $('.carousel-slides').slick({
     dots: true,
     arrows: true,
     slidesToShow: 1,
-    slidesToScroll: 1       
-})
+    slidesToScroll: 1               
+  
+           
+});
 
-   
+
+
+
+
