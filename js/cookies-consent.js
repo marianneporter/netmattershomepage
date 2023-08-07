290173ad8ad649958e05af0697c5bc6679e9d792
marianneporter <mariannneporter17@gmail.com>
2023-08-07 17:09:42 +0100

    added buttons for cookie consent and made a start on functionality

--- js/cookies-consent.js
+++ js/cookies-consent.js
@@ -1,14 +1,32 @@
-// const cookiesOverlay = document.querySelector('.cookies-overlay');
-// console.log(cookiesOverlay);
-// console.log('hello from cookies consent');
+const cookiesOverlay = document.querySelector('.cookies-overlay');
+const cookiesConsentPopup = document.querySelector('.cookies-consent');
+const acceptCookiesBtn = document.querySelector('.accept-cookies-btn');
 
-// console.log('adding no transition class');
-// cookiesOverlay.classList.add('no-transition');
-// cookiesOverlay.classList.add('fullpage-overlay');
-// cookiesOverlay.style.opacity ="0.6";
-// cookiesOverlay.style.zIndex = "10";  
-// cookiesOverlay.classList.remove('no-transition');
-// console.log('notransition class removed');
-// body.style.position = "fixed";
-// body.style.marginRight = "1rem";
+addOverlay();
 
+
+
+acceptCookiesBtn.addEventListener('click', () => {
+    console.log('accept cookies button clicked!');
+    cookiesConsentPopup.style.zIndex = "-999";
+    removeOverlay();
+});
+
+function addOverlay() {
+    console.log('adding no transition class');
+    cookiesOverlay.classList.add('no-transition');
+    cookiesOverlay.classList.add('fullpage-overlay');
+    cookiesOverlay.style.opacity ="0.6";
+    cookiesOverlay.style.zIndex = "10";  
+    cookiesOverlay.classList.remove('no-transition');
+    console.log('notransition class removed');
+}
+
+function removeOverlay() {
+  
+    cookiesOverlay.classList.add('no-transition');
+    cookiesOverlay.classList.remove('fullpage-overlay');
+    cookiesOverlay.style.opacity ="0";
+    cookiesOverlay.style.zIndex = "-999";  
+    cookiesOverlay.classList.remove('no-transition');
+}   
