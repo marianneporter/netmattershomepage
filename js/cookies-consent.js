a4e907c7b77beef9c4b8da4328147b6d0742fd3e
marianneporter <mariannneporter17@gmail.com>
2023-08-08 12:57:19 +0100

    wip cookie consent responsiveness

--- js/cookies-consent.js
+++ js/cookies-consent.js
@@ -1,29 +1,27 @@
 const cookiesOverlay = document.querySelector('.cookies-overlay');
 const cookiesConsentPopup = document.getElementById('cookies-consent');
 const acceptCookiesBtn = document.querySelector('.accept-cookies-btn');
-//const storageType = localStorage;
 const daysConsent = 30;
 const consentPropertyName = "nm-expiry-date";
 
-if (shouldShowPopup()) {
+if (shouldShowConsentPopup()) {
     console.log('should show popup is true');
         
     cookiesConsentPopup.style.zIndex = "999";
     
-    addOverlay();
+    addCookiesConsentOverlay();
 } else {
     console.log('should show popup is false');
 }
 
 
-acceptCookiesBtn.addEventListener('click', () => {
-    console.log('accept cookies button clicked!');
+acceptCookiesBtn.addEventListener('click', () => {   
     saveConsentToStorage();
     cookiesConsentPopup.style.zIndex = "-999";
-    removeOverlay();
+    removeCookiesConsentOverlay();
 });
 
-function shouldShowPopup() {
+function shouldShowConsentPopup() {
     const expiryDate = localStorage.getItem(consentPropertyName);
     if (expiryDate === null) {
         return true;
@@ -47,21 +45,18 @@ function saveConsentToStorage() {
     localStorage.setItem(consentPropertyName, expiryDate);
 }
 
-function addOverlay() {
-    console.log('adding no transition class');
+function addCookiesConsentOverlay() { 
     cookiesOverlay.classList.add('no-transition');
     cookiesOverlay.classList.add('fullpage-overlay');
     cookiesOverlay.style.opacity ="0.6";
     cookiesOverlay.style.zIndex = "10";  
-    cookiesOverlay.classList.remove('no-transition');
-    console.log('notransition class removed');
+    cookiesOverlay.classList.remove('no-transition');   
 }
 
-function removeOverlay() {
-    
-    cookiesOverlay.classList.add('no-transition');
+function removeCookiesConsentOverlay() {    
+ 
     cookiesOverlay.classList.remove('fullpage-overlay');
     cookiesOverlay.style.opacity ="0";
     cookiesOverlay.style.zIndex = "-999";  
-    cookiesOverlay.classList.remove('no-transition');
+   
 }   
