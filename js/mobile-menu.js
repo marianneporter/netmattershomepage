8ae626a15864094c6b679db79634abac7945b3c0
marianneporter <mariannneporter17@gmail.com>
2023-08-22 22:44:46 +0100

    wip mobile menu

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -14,15 +14,6 @@ hamburgerMenuButton.addEventListener('click', () => {
             mainContent.style.right = "21rem";
         }       
      
-        mainContent.style.position = "fixed";       
-        mobileMenu.style.overFlowY = "scroll";
-      
-
-        if (window.innerWidth > 480) {
-            mainContent.style.overflowY = "scroll";
-            disableScrolling(mainContent);
-        }
-
         contentOverlay.classList.add('content-overlay');
         contentOverlay.style.opacity ="0.6";
         contentOverlay.style.zIndex = "10";  
@@ -33,34 +24,20 @@ hamburgerMenuButton.addEventListener('click', () => {
 //contentOverlay has been clicked so page slides back over the mobile menu
 contentOverlay.addEventListener('click', (e) => {  
     
-  //  mainContent.style.right = "0";
+    mainContent.style.right = "0";
 
     hamburgerMenuButton.classList.remove("hamburger-menu-expanded");
-  
-    mobileMenu.style.overFlowY = "hidden";
-
-    if (window.innerWidth > 480 ) {
-        mainContent.style.overflowY = "auto";
-        enableScrolling(mainContent);
-    }
-  
+    
     // transition main content to the right and opacity of content 
     // overlay to zero via properties in the scss
     mainContent.style.right = "0";  
     contentOverlay.style.opacity = "0";
     setTimeout(() => {   
         //reset style of main content and opacity of overlay 
-        // after the CSS transitions have taken place  
-        mainContent.style.position ="relative";    
+        // after the CSS transitions have taken place        
         contentOverlay.style.zIndex = "-1";
         contentOverlay.classList.remove("content-overlay");  
         mobileMenu.style.display = "none";          
-    } , 1000);  
-
-  
-   
-
-    
-
+    } , 500);  
 });
 
