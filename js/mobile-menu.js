638516782a12f6a18482cc1b47ae570843d4cbf6
marianneporter <mariannneporter17@gmail.com>
2023-08-25 22:05:27 +0100

    wip sticky and mobile menus - resolved header not moving to right

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -7,12 +7,15 @@ hamburgerMenuButton.addEventListener('click', () => {
 
         if (window.innerWidth < 350) {
             mainContent.style.right = "13rem";
+            headerGroup.style.left = "-13rem";
             headerGroup.style.right = "13rem";
         } else if (window.innerWidth < 480) {
             mainContent.style.right = "17.5rem";
+            headerGroup.style.left = "-17.5rem";
             headerGroup.style.right = "17.5rem";
         } else {
             mainContent.style.right = "21rem";
+            headerGroup.style.left = "-21rem";
             headerGroup.style.right = "21rem";
         }       
      
@@ -23,8 +26,9 @@ hamburgerMenuButton.addEventListener('click', () => {
 
 //contentOverlay has been clicked so page slides back over the mobile menu
 contentOverlay.addEventListener('click', (e) => {   
-    console.log('overlay click envent fired');   
+    console.log('content overlay clicked!');   
     mainContent.style.right = "0";
+    headerGroup.style.left = "0";
     headerGroup.style.right = "0";
     removeContentOverlay();
     hamburgerMenuButton.classList.remove("hamburger-menu-expanded");
