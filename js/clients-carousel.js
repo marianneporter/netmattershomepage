fad9756c9475124c7893e6a82abcdb95145d5601
marianneporter <mariannneporter17@gmail.com>
2023-08-28 19:01:43 +0100

    added slick slider got clients carousel

--- js/clients-carousel.js
+++ js/clients-carousel.js
@@ -1,5 +1,15 @@
-const clientsCarouselItems = document.querySelectorAll('.clients-carousel-item');
+// $('.clients-carousel').slick({
+//     dots: false,
+//     arrows: false, 
+//     slidesToShow: 8,
+//     slidesToScroll: 1,
+//     autoplay: true,
+//     autoplaySpeed: 2000,
+//     pauseOnHover: true           
+// });
 
+
+const clientsCarouselItems = document.querySelectorAll('.clients-carousel-item');
 
  clientsCarouselItems.forEach(item => {
      let tooltipWrapper = item.querySelector('.tooltip-wrapper');
