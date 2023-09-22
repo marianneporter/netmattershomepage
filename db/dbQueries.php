8f128806bf42240237ab9e553571cd18723b8de3
marianneporter <marianneporter17@gmail.com>
2023-09-22 23:12:00 +0100

    wip adding data from db to latest news cards

--- db/dbQueries.php
+++ db/dbQueries.php
@@ -3,18 +3,19 @@
 class DB_Queries {
 
     public function getLatestNewsArticles($db) {
-      
-        $stmt  = $db->prepare("SELECT * FROM `latest_news`
-                               ORDER BY posted_by_date DESC
-                               LIMIT 3" );        
+   
         
-        try {
+        try {                  
+            $stmt  = $db->prepare("SELECT * FROM `latest_news`
+            ORDER BY posted_by_date DESC
+            LIMIT 3" );   
+
             $stmt->execute();         
             $articles = $stmt->fetchAll(PDO::FETCH_ASSOC); 
             return $articles;
         }
         catch(Exception $e) {
-            return null;
+            return [];
         }  
         
        
