4e400fbc35aeeecf82e13b99fbd006c2b5553942
marianneporter <marianneporter17@gmail.com>
2023-09-23 23:24:48 +0100

    wip formatting of office cards - completed for phones

--- contact-us.php
+++ contact-us.php
@@ -3,30 +3,31 @@
 ?>
 
 <div class="contact-page content">
-   <h2>Contact Nutmatters today!</h1>
+   
+   <h1>Our Offices</h2>
 
    <main>
       <div class="office-cards">
          <div class="office-card">
             <img src="images/offices/london.jpg" alt="">
-            <h4>London Office</h4>
-            <div class="address">
-               <p>Unit G6,</p>
-               <p>Pixel Business Centre,</p>
-               <p>110 Brooker Road, Waltham Abbey,</p>
-               <p>London,</p>
-               <p>EN9 1JH</p>
+            <div class="office-card-text">
+               <h4>London Office</h4>
+               <div class="address">
+                  <p>Unit G6,</p>
+                  <p>Pixel Business Centre,</p>
+                  <p>110 Brooker Road, Waltham Abbey,</p>
+                  <p>London,</p>
+                  <p>EN9 1JH</p>
+               </div>
+               <p class="phone">02045 397354</p>
+               <a class="view-more-btn" href="#">VIEW MORE</a>
             </div>
-            <p class="telephone">02045 397354</p>
-            <a class="view-more-btn" href="#">VIEW MORE</a>
+
             <div class="map-container">
                <iframe class="map-iframe" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11767.910425271419!2d-0.01523071646948471!3d51.68000233203411!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48761fe63fd90547%3A0x59ebdd0b2c47b21c!2sNetmatters%20London!5e0!3m2!1sen!2suk!4v1694613203146!5m2!1sen!2suk" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-            </div>
-            
+            </div>            
          </div>
       </div>
-
-
    </main>
 </div>
 
