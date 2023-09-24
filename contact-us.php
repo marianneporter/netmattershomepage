cbfe1612da0f975249c8835a1b48948cdd8212d0
marianneporter <marianneporter17@gmail.com>
2023-09-24 16:12:16 +0100

    wip contact page top cards done for all screen sizes

--- contact-us.php
+++ contact-us.php
@@ -3,8 +3,12 @@
 ?>
 
 <div class="contact-page content">
+
+   <div class="page-indicator">
+      <p><span>Home </span>/<span> Our Offices</span></p>
+   </div>
    
-   <h1>Our Offices</h2>
+   <h1>Our Offices</h1>
 
    <main>
       <div class="office-cards">
@@ -25,6 +29,70 @@
 
             <div class="map-container">
                <iframe class="map-iframe" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11767.910425271419!2d-0.01523071646948471!3d51.68000233203411!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48761fe63fd90547%3A0x59ebdd0b2c47b21c!2sNetmatters%20London!5e0!3m2!1sen!2suk!4v1694613203146!5m2!1sen!2suk" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>            
+         </div>
+
+         <div class="office-card">
+            <img src="images/offices/cambridge.jpg" alt="">
+            <div class="office-card-text">
+               <h4>Cambridge Office</h4>
+               <div class="address">
+                  <p>Unit 1.31,</p>
+                  <p>St John's Innovation Centre,</p>
+                  <p>Cowley Road, Milton,</p>
+                  <p>Cambridge,</p>
+                  <p>CB4 0WS</p>
+               </div>
+               <p class="phone">01223 37 57 72</p>
+               <a class="view-more-btn" href="#">VIEW MORE</a>
+            </div>
+
+            <div class="map-container">
+               <iframe class="map-iframe" 
+                       src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2443.4463841206734!2d0.15128277704004142!3d52.23527525727029!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47d8711469d7de59%3A0x4ad66f1b36a452da!2sNetmatters%20Cambridge!5e0!3m2!1sen!2suk!4v1694613141715!5m2!1sen!2suk" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>            
+         </div>
+
+         <div class="office-card">
+            <img src="images/offices/wymondham.jpg" alt="">
+            <div class="office-card-text">
+               <h4>Wymondham Office</h4>
+               <div class="address">
+                  <p>Unit 15,</p>
+                  <p>Penfold Drive,</p>
+                  <p>Gateway 11 Business Park,</p>
+                  <p>Wymondham, Norfolk</p>
+                  <p>NR18 0WZ</p>
+               </div>
+               <p class="phone">01603 70 40 20</p>
+               <a class="view-more-btn" href="#">VIEW MORE</a>
+            </div>
+
+            <div class="map-container">
+               <iframe class="map-iframe" 
+                       src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2424.644379747!2d1.1339954766845473!3d52.576042072076014!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47d9ddac8dba0b4b%3A0x9c77ffbfe7911dab!2sNetmatters!5e0!3m2!1sen!2suk!4v1692192981237!5m2!1sen!2suk" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>            
+         </div>
+
+         
+         <div class="office-card">
+            <img src="images/offices/yarmouth-2.jpg" alt="">
+            <div class="office-card-text">
+               <h4>Great Yarmouth Office</h4>
+               <div class="address">
+                  <p>Suite F23,</p>
+                  <p>Beacon Innovation Centre,</p>
+                  <p>Beacon Park, Gorleston,</p>
+                  <p>Great Yarmouth, Norfolk</p>
+                  <p>NR31 7RA</p>
+               </div>
+               <p class="phone">01492 60 32 04</p>
+               <a class="view-more-btn" href="#">VIEW MORE</a>
+            </div>
+
+            <div class="map-container">
+               <iframe class="map-iframe" 
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1715.2766080459753!2d1.712628985995402!3d52.55573141109476!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47da0593b311cec3%3A0x1cb3c1d4c0b340f6!2sNetmatters%20Great%20Yarmouth!5e0!3m2!1sen!2suk!4v1692193267187!5m2!1sen!2suk" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>            
          </div>
       </div>
